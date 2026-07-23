73e8911c45faf4b85417ea0be3e90c4cd52667a0
LilPrinz3 <bakreibrahim02@gmail.com>
2026-07-23 23:35:36 +0300

    Fix Gmail SMTP IPv4 connection

--- src/controller/paymentController.js
+++ src/controller/paymentController.js
@@ -39,6 +39,7 @@ const sendBookingEmails = async (booking) => {
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
+    family: 4,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
